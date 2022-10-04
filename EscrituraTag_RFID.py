83a1ae69d473b2cfdd2c03ab6988bffc1bb3d890
Ramsés Ortiz Castro <Ramses.ortiz36@gmail.com>
2022-10-03 22:01:48 -0700

    Se agregan los comentarios al codigo

--- EscrituraTag_RFID.py
+++ EscrituraTag_RFID.py
@@ -1,14 +1,18 @@
+#Bibliotecas 
+
 from cgitb import text
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
+#iniciar sensor
 reader = SimpleMFRC522()
 
+#ciclo de control para detectar errores try:
 try:
-    text = input('New data: ')
-    print("Now place your tag to write")
-    reader.write(text)
-    print("Written")
+    text = input('New data: ') #Solicitar que se va escribir en el tag y guardarlo en variable text
+    print("Now place your tag to write")    #Mensaje al usaurio, instruccion de acercar el tag
+    reader.write(text)  #función reader lee y escribe en el tag, en este caso solo escribe
+    print("Written")    #mensaje al usuario de finalizado de proceso de escritura
 
-finally:
-    GPIO.cleanup()
+finally:    #En caso de terminar realiza lo siguiente
+    GPIO.cleanup()  #Limpia los puertos GPIO
