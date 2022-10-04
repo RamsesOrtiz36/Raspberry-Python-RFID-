769bca76eced82450958ac2bad93cc612ead51ae
Ramsés Ortiz Castro <Ramses.ortiz36@gmail.com>
2022-10-04 12:20:23 -0700

    Prueba de conexion

--- TestConexionMySQL.py
+++ TestConexionMySQL.py
@@ -1,4 +1,9 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='Ramses', password='1234', host='192.168.1.68', database='codigoIoT')
+cnx = mysql.connector.connect(user='Ramses', password='Tanis36', host='192.168.1.68', database='CodigoIoT')
+print("conexion realizada")
+print(cnx)
+
+print ("Cerrar conexión")
 cnx.close()
+print ("Conexion cerrada")
