dde365379e4762f64d9378a2d164f30f35aad815
Ramsés Ortiz Castro <Ramses.ortiz36@gmail.com>
2022-10-20 12:37:57 -0700

    Actualizar la com en nivel de acceso del text

--- EscrituraTag_RFID.py
+++ EscrituraTag_RFID.py
@@ -14,7 +14,7 @@ try:
     Celular = input('Numero de celular del usuario: ')
     Fecha_Naciemiento = input('Fecha de nacimiento del usuario: ')
     Nivel_acceso = input('Nivel de acceso del usuario: ')
-    text = (Nombre + ',' + str(Edad) +','+ str(Celular) +','+ str(Fecha_Naciemiento) +','+ str(Nivel_acceso))
+    text = (Nombre + ',' + str(Edad) +','+ str(Celular) +','+ str(Fecha_Naciemiento) +','+ str(Nivel_acceso)+',')
     print("Acerca la etiqueta RF o tarjeta RF al sensor RF")    #Mensaje al usaurio, instruccion de acercar el tag
     reader.write(text)  #función reader lee y escribe en el tag, en este caso solo escribe
     print("Ya se escribio el texto en etiqueta RF o Tarjeta RF")    #mensaje al usuario de finalizado de proceso de escritura
