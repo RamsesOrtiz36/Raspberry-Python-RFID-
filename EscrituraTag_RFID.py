55171183e086e6536e31d77bb4355678f57b3799
Ramsés Ortiz Castro <Ramses.ortiz36@gmail.com>
2022-10-20 11:23:45 -0700

    Pide datos al usario y los escribe en el tag

--- EscrituraTag_RFID.py
+++ EscrituraTag_RFID.py
@@ -9,7 +9,12 @@ reader = SimpleMFRC522()
 
 #ciclo de control para detectar errores try:
 try:
-    text = input('New data: ') #Solicitar que se va escribir en el tag y guardarlo en variable text
+    Nombre = input('Nombre del usuario: ')
+    Edad = input('Edad del usuario: ')
+    Celular = input('Numero de celular del usuario: ')
+    Fecha_Naciemiento = input('Fecha de nacimiento del usuario: ')
+    Nivel_acceso = input('Nivel de acceso del usuario: ')
+    text = (Nombre + ',' + str(Edad) +','+ str(Celular) +','+ str(Fecha_Naciemiento) +','+ str(Nivel_acceso))
     print("Acerca la etiqueta RF o tarjeta RF al sensor RF")    #Mensaje al usaurio, instruccion de acercar el tag
     reader.write(text)  #función reader lee y escribe en el tag, en este caso solo escribe
     print("Ya se escribio el texto en etiqueta RF o Tarjeta RF")    #mensaje al usuario de finalizado de proceso de escritura
